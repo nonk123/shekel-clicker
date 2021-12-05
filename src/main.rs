a42730270a4c2ecb6f5cf8d1c14e9ffe8ec907fb
nonk123 <Sergeyii321@gmail.com>
2021-12-05 08:49:33 +0300

    Upgrade the UI
    The list of buildings is now a table. Its view has also been moved into
    a separate function.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod building;
 use building::Building;
 
 const SHEKEL: &str = "₪";
+const SHEKELS_PER_SECOND: &str = " ₪/s";
 
 enum Msg {
     ShekelClicked,
@@ -46,7 +47,7 @@ impl Component for Model {
                 Building::new(
                     "Yewish Commune".to_string(),
                     BigUint::from(10u32),
-                    BigUint::from(500u32),
+                    BigUint::from(400u32),
                 ),
                 Building::new(
                     "Yewish Town".to_string(),
@@ -95,66 +96,76 @@ impl Component for Model {
 
     fn view(&self) -> Html {
         html! {
-            <div style="display: flex;">
-                <div style="width: 50%;">
+            <div>
+                <div class="shekel-container">
                     <button onclick=self.link.callback(|_| Msg::ShekelClicked)
                             class="shekel-button">
                         <img draggable="false" style="width: 100%" src="/shekel.png"/>
                     </button>
                     <div class="shekel-count">{ self.shekel_count.to_string() } { SHEKEL }</div>
                 </div>
-                <div class="upgrades">
-                    { for self.building_types.iter().enumerate().map(|(index, building)| {
-                        let buy = move |count: BigUint| {
-                            self.link
-                                .callback(move |_| Msg::BuildingBought(index, count.clone()))
-                        };
-
-                        html! {
-                            <div class="upgrade">
-                                <div>
-                                    <div>{ building.name.to_string() }</div>
-                                    <div>
-                                        { "Owned: " }
-                                        { building.count.to_string() }
-                                    </div>
-                                    <div>
-                                        { "Cost: " }
-                                        { building.cost.to_string() }
-                                        { SHEKEL }
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="center-text">
-                                        { "Income: " }
-                                        { building.shekels_per_second.to_string() }
-                                        { " " }
-                                        { SHEKEL }
-                                        { "/s" }
-                                    </div>
-                                    <div class="center-text">
-                                        { "Total: " }
-                                        { building.calculate_income().to_string() }
-                                        { " " }
-                                        { SHEKEL }
-                                        { "/s" }
-                                    </div>
-                                </div>
-                                { for [1u32, 10, 50, 100, 500].iter().map(|count| {
-                                    let count = BigUint::from(*count);
-
-                                    html! {
-                                        <button class="buy-button" onclick=buy(count.clone())>
-                                            { count.to_string() }
-                                        </button>
-                                    }
-                                }) }
-                            </div>
-                        }
-                    }) }
+                <div>
+                    <h1>{ "Statistics" }</h1>
+                    <div>{ "Income: " } { self.calculate_total_income() } { SHEKELS_PER_SECOND }</div>
+                    <h1>{ "Buildings" }</h1>
+                    <table class="upgrades">
+                        { for self.building_types.iter().enumerate().map(|(index, building)| {
+                            self.view_building_upgrade(index, building)
+                        }) }
+                    </table>
                 </div>
             </div>
         }
+    }
+}
+
+impl Model {
+    fn view_building_upgrade(&self, index: usize, building: &Building) -> Html {
+        let buy = move |count: BigUint| {
+            self.link
+                .callback(move |_| Msg::BuildingBought(index, count.clone()))
+        };
+
+        html! {
+            <tr>
+                <td>{ building.name.to_string() }</td>
+                <td>{ "Cost: " } { building.cost.to_string() } { SHEKEL }</td>
+                <td class="center-text">
+                    { "Income: " }
+                    { building.shekels_per_second.to_string() }
+                    { " " }
+                    { SHEKEL }
+                    { "/s" }
+                </td>
+                <td>{ "Owned: " } { building.count.to_string() }</td>
+                <td class="center-text">
+                    { "Total: " }
+                    { building.calculate_income().to_string() }
+                    { SHEKELS_PER_SECOND }
+                </td>
+                { for [1u32, 10, 50, 100, 500].iter().map(|count| {
+                    let count = BigUint::from(*count);
+
+                    html! {
+                        <td>
+                            <button class="buy-button" onclick=buy(count.clone())>
+                                { count.to_string() }
+                            </button>
+                        </td>
+                    }
+                }) }
+            </tr>
+        }
+    }
+
+    fn calculate_total_income(&self) -> BigUint {
+        let mut income = BigUint::from(0u32);
+
+        for building in &self.building_types {
+            income = &income + building.calculate_income();
+        }
+
+        income
     }
 }
 
